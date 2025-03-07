0dbacc9645bc3263e7f888f5eabc7937aa15ce46
njhuffman <nathan.huffman2016@gmail.com>
2025-03-07 06:10:46 +0000

    Use sha for keys in consistency test
    The keys need to be cknsistent, not correct. Reduced
    test time from many minutes to 3

--- consistency_test.go
+++ consistency_test.go
@@ -7,7 +7,22 @@ import (
 	"log"
 	mrand "math/rand/v2"
 	"testing"
+	"golang.org/x/crypto/sha3"
 )
+
+func shaArgonKey(password string, salt [16]byte, iterations uint32, parallelism uint8) [32]byte{
+	// faster stand-in for testing
+	data := append(append(append([]byte(password), salt[:]...), byte(iterations)), byte(parallelism))
+	hasher := sha3.New256()
+	_, err := hasher.Write(data)
+	if err != nil {
+		panic(err)
+	}
+	key := [32]byte{}
+	copy(key[:], hasher.Sum(nil))
+	return key
+}
+
 
 func allSettings() []Settings {
 	settings := []Settings{}
@@ -112,6 +127,7 @@ func testConsistency(settings Settings, size int, numKeyfiles int, t *testing.T)
 
 func TestConsistencyEmptyFile(t *testing.T) {
 	// test for an empty file
+	argonKey = shaArgonKey
 	for _, settings := range allSettings() {
 		for numKeyfiles := range 3 {
 			testConsistency(settings, 0, numKeyfiles, t)
@@ -121,6 +137,7 @@ func TestConsistencyEmptyFile(t *testing.T) {
 
 func TestConsistencySmallFile(t *testing.T) {
 	// test for a file size less than readSize
+	argonKey = shaArgonKey
 	for _, settings := range allSettings() {
 		for numKeyfiles := range 3 {
 			testConsistency(settings, readSize/2, numKeyfiles, t)
@@ -130,6 +147,7 @@ func TestConsistencySmallFile(t *testing.T) {
 
 func TestConsistencyLargeFile(t *testing.T) {
 	// test for a file size greater than readSize
+	argonKey = shaArgonKey
 	for _, settings := range allSettings() {
 		for numKeyfiles := range 3 {
 			testConsistency(settings, readSize*2+500, numKeyfiles, t)
@@ -139,6 +157,7 @@ func TestConsistencyLargeFile(t *testing.T) {
 
 func TestConsistencyReadSize(t *testing.T) {
 	// test for a file exactly at readSize
+	argonKey = shaArgonKey
 	for i, settings := range allSettings() {
 		for numKeyfiles := range 3 {
 			testConsistency(settings, readSize, numKeyfiles, t)
